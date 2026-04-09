bf3f5a6b3d168810c9602ad8de2dc5385be0647e
carsonroscoe <carsonroscoe7@gmail.com>
2026-04-09 16:30:30 -0700

    fix: hono/fastify had tests referencing /dist

--- typescript/packages/http/hono/vitest.config.ts
+++ typescript/packages/http/hono/vitest.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,4 +8,16 @@ export default defineConfig(({ mode }) => ({
     env: loadEnv(mode, process.cwd(), ""),
   },
   plugins: [tsconfigPaths({ projects: ["."] })],
+  resolve: {
+    alias: [
+      {
+        find: /^@x402\/core\/(.+)/,
+        replacement: path.resolve(__dirname, "../../core/src/$1/index.ts"),
+      },
+      {
+        find: /^@x402\/core$/,
+        replacement: path.resolve(__dirname, "../../core/src/index.ts"),
+      },
+    ],
+  },
 }));
